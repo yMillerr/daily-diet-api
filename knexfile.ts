0b195953057fdb2def02355ea2750c73b69cfd21
ymillerr <millerandvitor@gmail.com>
2023-11-30 17:21:35 +0100

    chore: create database connection

---
+++ knexfile.ts
@@ -0,0 +1,3 @@
+import { config } from "./src/databaseConfig";
+
+export default config
---
+++ src/databaseConfig.ts
@@ -0,0 +1,16 @@
+import { knex as setupKnex, Knex } from 'knex'
+
+export const config: Knex.Config = {
+  client: 'sqlite3',
+  connection: {
+    filename: './db/app.db'
+  },
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations'
+  },
+  useNullAsDefault: true
+}
+
+export const knex = setupKnex(config)
+
