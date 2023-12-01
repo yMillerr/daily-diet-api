9f93b385a30eb8a69ca892eec77ae2efc77848fc
ymillerr <millerandvitor@gmail.com>
2023-12-01 18:00:25 +0100

    feat: create knex tables types

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,25 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      name: string
+      email: string
+      password: string
+      avatar_url?: string
+      created_at: string
+      session_id?: string
+    }
+    meals: {
+      id: string
+      name: string
+      category: string
+      description?: stringç
+      created_at: string
+      updated_at: string
+      user_id: string
+    }
+  }
+}
