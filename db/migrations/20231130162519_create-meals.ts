5162bd567e718f31162b36acafe93a301008ed47
ymillerr <millerandvitor@gmail.com>
2023-12-01 19:11:29 +0100

    feat: add date & hour at migration

--- db/migrations/20231130162519_create-meals.ts
+++ db/migrations/20231130162519_create-meals.ts
@@ -5,9 +5,12 @@ export async function up(knex: Knex): Promise<void> {
     table.uuid('id').primary()
     table.text('name').notNullable()
     table.text('description')
+    table.text('category')
+    table.text('date').notNullable()
+    table.text('hour').notNullable()
+
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
     table.timestamp('updated_at')
-    table.text('category')
     table.text('user_id').notNullable().index()
   })
 }
